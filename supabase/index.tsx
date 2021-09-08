d6bbb844c53875fabd7a8d8201635d67660d6d24
Mayank_Sh07 <52369953+Mayank-Sh07@users.noreply.github.com>
2021-09-08 15:24:21 +0530

    Update index.tsx

--- supabase/index.tsx
+++ supabase/index.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "key_here",
-  "Key_here"
+  "https://bcqincllvupgnvfhfldh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTg1NzcwOCwiZXhwIjoxOTM1NDMzNzA4fQ.YRYiFpLIvlP-e9R0gvDIoo3_cWA1qcjThLARVrfE_58"
 );
